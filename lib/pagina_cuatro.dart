7278de7538ec7a83b66b4a0c78e5818e6c15ce39
Luis_Sifuentes <a.22308051281313@cbtis128.edu.mx>
2025-05-02 18:43:34 +0000

    lulu

---
+++ lib/pagina_cuatro.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PantallaCuatro extends StatefulWidget {
+  const PantallaCuatro({Key? key}) : super(key: key);
+
+  @override
+  State<PantallaCuatro> createState() => _Widget006State();
+}
+
+class _Widget006State extends State<PantallaCuatro> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          height: 250.0,
+          color: Colors.blueGrey,
+          child: AnimatedAlign(
+            alignment: selected ? Alignment.topRight : Alignment.bottomLeft,
+            duration: const Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
+            child: const FlutterLogo(size: 50.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
